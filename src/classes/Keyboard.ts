d597ed97981f453ab053a9463949d5941e29c1d5
rulewrite <rulewrite@gmail.com>
2021-02-05 19:42:20 +0900

    feat: 상단 키 추가

--- src/classes/Keyboard.ts
+++ src/classes/Keyboard.ts
@@ -2,9 +2,11 @@ class Keyboard {
   private static readonly KEYDOWN_EVENT_TYPE = 'keydown';
   private static readonly SET_LEFT_KEY = new Set(['Left', 'ArrowLeft']);
   private static readonly SET_RIGHT_KEY = new Set(['Right', 'ArrowRight']);
+  private static readonly SET_UP_KEY = new Set(['Up', 'ArrowUp']);
 
   isPressedLeft = false;
   isPressedRight = false;
+  isPressedUp = false;
   get isPressedMovingKey() {
     return this.isPressedLeft || this.isPressedRight;
   }
@@ -21,12 +23,14 @@ class Keyboard {
 
     if (Keyboard.SET_LEFT_KEY.has(key)) {
       this.isPressedLeft = isPressed;
-      return;
     }
 
     if (Keyboard.SET_RIGHT_KEY.has(key)) {
       this.isPressedRight = isPressed;
-      return;
+    }
+
+    if (Keyboard.SET_UP_KEY.has(key)) {
+      this.isPressedUp = isPressed;
     }
   }
 }
