46384706be7dd2dcb6220c97803edc0771d98326
rulewrite <rulewrite@gmail.com>
2021-02-10 00:20:35 +0900

    feat: 스크롤 클래스 속도 속성 추가

--- src/classes/Scroll.ts
+++ src/classes/Scroll.ts
@@ -1,9 +1,13 @@
 import Platform from './Platform';
 
 class Scroll {
+  private static readonly INITIAL_SPEED = 1;
+
+  constructor(private speed: number = Scroll.INITIAL_SPEED) {}
+
   wind(platforms: Platform[]) {
     platforms.forEach((platform) => {
-      platform.y = platform.y - 1;
+      platform.y = platform.y - this.speed;
     });
   }
 }
