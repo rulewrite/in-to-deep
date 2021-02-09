b6d760272e28adda4a73379e129850b16497206d
rulewrite <rulewrite@gmail.com>
2021-02-10 00:20:17 +0900

    chore: 미사용 키보드 속성 제거

--- src/classes/Game.ts
+++ src/classes/Game.ts
@@ -1,5 +1,4 @@
 import Gravity from '@classes/Gravity';
-import Keyboard from '@classes/Keyboard';
 import Hero from '@classes/Hero';
 import Scroll from '@classes/Scroll';
 import Obstacles from '@classes/Obstacles';
@@ -10,8 +9,6 @@ import Environment from './Environment';
 import Controller from './Controller';
 
 export default class Game {
-  private static readonly KEYBOARD = new Keyboard();
-
   private readonly HERO = new Hero(60, 30, 30, 30);
   private readonly AREA;
   private readonly GRAVITY = new Gravity(this.HERO);
