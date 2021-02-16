358e3c471c12ef366b1c0f66ea191ee4a3b89309
rulewrite <rulewrite@gmail.com>
2021-02-17 03:45:12 +0900

    refactor: drawable 묶음, 불필요 속성 제거

--- src/classes/Game.ts
+++ src/classes/Game.ts
@@ -14,14 +14,22 @@ export default class Game implements Drawable {
   private readonly GRAVITY = new Gravity(this.HERO);
   private readonly CYCLE_OBSTACLES;
   private readonly CONTROLLER = new Controller(this.HERO);
-  private readonly OVERFLOW_GUIDE = new OverflowGuide(this.HERO);
-  private readonly DEBUGGER;
+  private readonly DRAWABLES: Drawable[];
 
   constructor(width: number, height: number) {
     this.AREA = new Area(width, height, this.HERO);
     this.CYCLE_OBSTACLES = new CycleObstacles(width, height, this.HERO.width);
 
-    this.DEBUGGER = new Debugger(this.CYCLE_OBSTACLES, this.AREA, this.HERO);
+    this.DRAWABLES = [
+      this.HERO,
+      this.CYCLE_OBSTACLES,
+      new OverflowGuide(this.HERO),
+    ];
+    if (Environment.IS_DEVELOPMENT) {
+      this.DRAWABLES.push(
+        new Debugger(this.CYCLE_OBSTACLES, this.AREA, this.HERO)
+      );
+    }
   }
 
   isOver() {
@@ -47,13 +55,6 @@ export default class Game implements Drawable {
   draw(context: CanvasRenderingContext2D) {
     this.update();
 
-    this.HERO.draw(context);
-    this.CYCLE_OBSTACLES.draw(context);
-    this.OVERFLOW_GUIDE.draw(context);
-
-    // debug
-    if (Environment.IS_DEVELOPMENT) {
-      this.DEBUGGER.draw(context);
-    }
+    this.DRAWABLES.forEach((drawable) => drawable.draw(context));
   }
 }
