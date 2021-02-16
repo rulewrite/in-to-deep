ce0815352c839e01acc2924cd9eb9bdc7c77ee72
rulewrite <rulewrite@gmail.com>
2021-02-17 00:07:48 +0900

    chore: 스크롤 wind 메소드 인자 자료형 변경

--- src/classes/Scroll.ts
+++ src/classes/Scroll.ts
@@ -1,13 +1,13 @@
-import Platform from './Platform';
+import Shape from './Shape';
 
 export default class Scroll {
   private static readonly INITIAL_SPEED = 1;
 
   constructor(private speed: number = Scroll.INITIAL_SPEED) {}
 
-  wind(platforms: Platform[]) {
-    platforms.forEach((platform) => {
-      platform.y = platform.y - this.speed;
+  wind(shapes: Shape[]) {
+    shapes.forEach((shape) => {
+      shape.y -= this.speed;
     });
   }
 }
